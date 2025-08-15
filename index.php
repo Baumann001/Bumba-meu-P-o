de9143ca958c9f29314dcfc51221c33728a946a1
Baumann001 <gustavo_baumann@estudante.sesisenai.org.br>
2025-08-15 09:59:24 -0300

    add-

--- index.php
+++ index.php
@@ -9,12 +9,3 @@
 <h1>🍞 Cardápio da Padaria</h1>
 <p>Escolha seu produto favorito:</p>
 
-<form method="POST" action="">
-    <input type="checkbox" name="produto[]" value="Pão francês"> Pão francês - R$ 0,50<br>
-    <input type="checkbox" name="produto[]" value="Café"> Café - R$ 2,00<br>
-    <input type="checkbox" name="produto[]" value="Bolo de chocolate"> Bolo de chocolate - R$ 5,00<br>
-    <input type="checkbox" name="produto[]" value="Coxinha"> Coxinha - R$ 4,50<br>
-    <input type="checkbox" name="produto[]" value="Pastel"> Pastel - R$ 3,50<br><br>
-
-    <input type="submit" value="Fazer pedido">
-</form>
