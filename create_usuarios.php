74ef15bf6c11140fedb74f71122f72df1448bd07
Baumann001 <gustavo_baumann@estudante.sesisenai.org.br>
2025-08-15 10:09:31 -0300

    add-

--- create_usuarios.php
+++ create_usuarios.php
@@ -10,9 +10,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $endereco = $_POST["endereco"];
     $data_contratacao = $_POST["data_contratacao"];
    
-  
-
-
     $sql = " INSERT INTO usuarios (nome,email,senha_hash,telefone,endereco,data_contratacao) VALUE ('$nome','$email','$senha','$telefone','$endereco','$data_contratacao')";
 
 
@@ -27,4 +24,32 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 
 ?>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Create</title>
+</head>
+
+<body>
+
+    <form method="POST" action="create.php">
+
+        <label for="name">Nome:</label>
+        <input type="text" name="name" required>
+
+        <label for="email">Email:</label>
+        <input type="email" name="email" required>
+
+        <input type="submit" value="Adicionar">
+
+    </form>
+
+    <a href="read.php">Ver registros.</a>
+
+</body>
+
+</html>
     
