06a6c42ff1caf88311ef17fb71d94404194f12b8
Baumann001 <gustavo_baumann@estudante.sesisenai.org.br>
2025-08-15 10:02:59 -0300

    add-

--- index.php
+++ index.php
@@ -2,10 +2,11 @@
 <head>
     <meta charset="UTF-8">
     <title>Cardápio da Padaria</title>
+
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-    
+
 
 <h1>🍞 Cardápio da Padaria</h1>
 <p>Escolha seu produto favorito:</p>
