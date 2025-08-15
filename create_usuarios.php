762eb311d18c3f28263fe0e0006267e83e1b8aba
Baumann001 <gustavo_baumann@estudante.sesisenai.org.br>
2025-08-15 09:39:06 -0300

    add-

--- create_usuarios.php
+++ create_usuarios.php
@@ -4,12 +4,18 @@ include 'db.php';
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nome = $_POST["nome"];
     $email = $_POST["email"];
-   
+    $id_usuario = $_POST["id_usuario"];
+    $senha = $_POST["senha"];
+    $telefone = $_POST["telefone"];
+    $endereco = $_POST["endereco"];
     $data_contratacao = $_POST["data_contratacao"];
+   
+  
 
 
-    $sql = " INSERT INTO usuarios (name,email) VALUE ('$name','$email')";
+    $sql = " INSERT INTO usuarios (nome,email,senha_hash,telefone,endereco,data_contratacao) VALUE ('$nome','$email','$senha','$telefone','$endereco','$data_contratacao')";
 
+    
     if ($conn->query($sql) === true) {
         echo "Novo registro criado com sucesso.";
     } else {
